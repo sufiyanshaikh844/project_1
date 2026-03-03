4a473e38f8fde7e18062236e90a3c8883079efbd
sufiyanshaikh844 <pcuse389@gmail.com>
2026-03-03 12:20:34 +0500

    data (ingestion,validation,transformation)

---
+++ src/datascience/entity/config_entity.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+@dataclass
+class DataValidationConfig:
+    root_dir:Path
+    STATUS_FILE:str
+    unzip_data_dir:Path
+    all_schema:dict
+
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
